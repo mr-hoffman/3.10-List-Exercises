99fa5d4eeefa2d4e0e2b7d12431e6a9ef9631fea
mr-hoffman <hoffmant@hilbertk12.org>
2025-02-07 10:48:50 -0600

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -1,1 +1,46 @@
-# 1: 
+# 1: Check if list is empty
+
+def empty_list(_list):
+  pass
+
+
+#2: Remove duplicates from list
+def remove_duplicates(_list):
+  pass
+
+
+#3: Find words longer than n
+def longer_words(_list):
+  pass
+
+
+#4: Check common members between two lists:
+def commom_words(_list1, _list2, num):
+  pass
+
+
+############### Create some lists here to test your functions ################
+############### Make at least two different lists to test each ###############
+
+to_test_empty_1 = []
+to_test_empty_2 = []
+
+to_test_remove_1 = []
+to_test_remove_2 = []
+
+to_test_longer_1 = []
+to_test_longer_2 = []
+
+to_test_common_1 = []
+to_test_common_2 = []
+
+############### Call your functions and test them here ################
+########### Add more function calls to test additonal lists ###########
+############### Print the output of your function calls ###############
+
+emp1 = empty_list(to_test_empty_1)
+emp2 = empty_list(to_test_empty_2)
+
+# finish writing tests: function calls and print statements
+
+
